7f032f2e2010fb8e71ff3aea306c846f7716bed2
Gayasri72 <gayasripethum1@gmail.com>
2024-04-21 07:19:16 +0530

    complete profile page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
-import { Profile } from "./pages/Profile";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Spaces } from "./pages/Spaces";
@@ -11,6 +10,7 @@ import { Packages } from "./pages/Packages";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import PrivateRoute from './components/PrivateRoute';
+import Profile from "./pages/Profile";
 
 export const App = () => {
   return (
