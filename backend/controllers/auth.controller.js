507ba8de6b4d18e5b0821fa72ea92680b6ca0c4f
Gayasri72 <gayasripethum1@gmail.com>
2024-04-22 10:54:35 +0530

    DB UPDATES

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -5,6 +5,16 @@ import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
   try {
