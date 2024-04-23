f85b98eda07636c18c0cb204f61cc2bc4035a9bc
Gayasri72 <gayasripethum1@gmail.com>
2024-04-23 06:56:01 +0530

    create get users api

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema(
     },
     isAdmin: {
       type: Boolean,
-      required: true,
+      
       default: false,
     },
   },
--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -4,8 +4,9 @@ import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
-
+import { useSelector } from 'react-redux';
 export default function DashSidebar() {
+  const { currentUser } = useSelector((state) => state.user);
   const location = useLocation();
   const dispatch = useDispatch();
   const [tab, setTab] = useState('');
@@ -41,7 +42,7 @@ export default function DashSidebar() {
             <Sidebar.Item
               active={tab === 'profile'}
               icon={HiUser}
-              label={'User'}
+              label={currentUser.isAdmin?'Admin':'User'}
               labelColor='dark'
               as='div'
             >
