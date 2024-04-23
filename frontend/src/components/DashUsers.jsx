7770bcf98cf71596d969b40c175a2479359f52db
Gayasri72 <gayasripethum1@gmail.com>
2024-04-23 08:42:04 +0530

    updates

--- frontend/src/components/DashUsers.jsx
+++ frontend/src/components/DashUsers.jsx
@@ -140,7 +140,7 @@ export default function DashUsers() {
             </h3>
             <div className='flex justify-center gap-4'>
               <Button color='failure' onClick={handleDeleteUser}>
-                Yes, I'm sure
+                Yes, Im sure
               </Button>
               <Button color='gray' onClick={() => setShowModal(false)}>
                 No, cancel
